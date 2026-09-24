93ec930a0ebe0374812c147e3d66805d69d14a8a
Rhys <e3e0261f@Pm.me>
2026-09-25 07:03:29 +0800

    Save config: 2026-09-25 07:03:29

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -336,7 +336,11 @@
       input = {
         accel_profile = "flat", -- 絕對直線，無軟體加速
         kb_layout  = "us",
-        follow_mouse = 0,
+        -- 0：必須點擊滑鼠 才能激活視窗（點擊激活，之前你的配置裡設成了 0）。
+        -- 1（最常用 ⭐️）：滑鼠移到哪裡，焦點就自動切換到哪個視窗（懸停即激活）。
+        -- 2：分離模式（鍵盤焦點與滑鼠焦點分離，點擊才鎖定鍵盤輸入）。
+        -- 3：完全點擊模式。
+        follow_mouse = 2,
         sensitivity = 0,
         touchpad = { natural_scroll = false },
       },
